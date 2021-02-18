42b2b1c6c3730b0365200e0e1d26e65559062b9d
ronaudinho <doelaudi@gmail.com>
2021-02-18 12:50:05 +0700

    rewrite test with testify

--- pkg/gomod/parse_test.go
+++ pkg/gomod/parse_test.go
@@ -4,72 +4,49 @@ import (
 	"os"
 	"path"
 	"sort"
-	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
 	"github.com/aquasecurity/go-dep-parser/pkg/types"
-	"github.com/kylelemons/godebug/pretty"
 )
 
 func TestParse(t *testing.T) {
 	vectors := []struct {
-		file      string
-		libraries []types.Library
+		file string
+		want []types.Library
 	}{
 		{
-			file:      "testdata/gomod_normal.sum",
-			libraries: GoModNormal,
+			file: "testdata/gomod_normal.sum",
+			want: GoModNormal,
 		},
 		{
-			file:      "testdata/gomod_many.sum",
-			libraries: GoModMany,
+			file: "testdata/gomod_many.sum",
+			want: GoModMany,
 		},
 		{
-			file:      "testdata/gomod_trivy.sum",
-			libraries: GoModTrivy,
+			file: "testdata/gomod_trivy.sum",
+			want: GoModTrivy,
 		},
 	}
 
 	for _, v := range vectors {
 		t.Run(path.Base(v.file), func(t *testing.T) {
 			f, err := os.Open(v.file)
-			if err != nil {
-				t.Fatalf("Open() error: %v", err)
-			}
-			libList, err := Parse(f)
-			if err != nil {
-				t.Fatalf("Parse() error: %v", err)
-			}
+			require.NoError(t, err)
 
-			sort.Slice(libList, func(i, j int) bool {
-				ret := strings.Compare(libList[i].Name, libList[j].Name)
-				if ret == 0 {
-					return libList[i].Version < libList[j].Version
-				}
-				return ret < 0
+			got, err := Parse(f)
+			require.NoError(t, err)
+
+			sort.Slice(got, func(i, j int) bool {
+				return got[i].Name < got[j].Name
+			})
+			sort.Slice(v.want, func(i, j int) bool {
+				return v.want[i].Name < v.want[j].Name
 			})
 
-			sort.Slice(v.libraries, func(i, j int) bool {
-				ret := strings.Compare(v.libraries[i].Name, v.libraries[j].Name)
-				if ret == 0 {
-					return v.libraries[i].Version < v.libraries[j].Version
-				}
-				return ret < 0
-			})
-
-			if len(libList) != len(v.libraries) {
-				t.Fatalf("lib length: %s", pretty.Compare(libList, v.libraries))
-			}
-
-			for i, got := range libList {
-				want := v.libraries[i]
-				if want.Name != got.Name {
-					t.Errorf("%d: Name: got %s, want %s", i, got.Name, want.Name)
-				}
-				if want.Version != got.Version {
-					t.Errorf("%d: Version: got %s, want %s", i, got.Version, want.Version)
-				}
-			}
+			assert.Equal(t, v.want, got)
 		})
 	}
 }
