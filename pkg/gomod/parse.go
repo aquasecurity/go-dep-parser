a71b57171f4a344813ef91d192abdc9124382dbb
ronaudinho <doelaudi@gmail.com>
2021-02-19 08:44:48 +0700

    wrap errors

--- pkg/gomod/parse.go
+++ pkg/gomod/parse.go
@@ -6,12 +6,13 @@ import (
 	"strings"
 
 	"github.com/aquasecurity/go-dep-parser/pkg/types"
+	"golang.org/x/xerrors"
 )
 
 // Parse parses a go.sum file
 func Parse(r io.Reader) ([]types.Library, error) {
 	var libs []types.Library
-	tmp := make(map[string]string)
+	uniqueLibs := make(map[string]string)
 
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
@@ -19,13 +20,13 @@ func Parse(r io.Reader) ([]types.Library, error) {
 		s := strings.Fields(line)
 		// go.sum records and sorts all non-major versions
 		// with the latest version as last entry
-		tmp[s[0]] = strings.TrimSuffix(strings.TrimPrefix(s[1], "v"), "/go.mod")
+		uniqueLibs[s[0]] = strings.TrimSuffix(strings.TrimPrefix(s[1], "v"), "/go.mod")
 	}
 	if err := scanner.Err(); err != nil {
-		return nil, err
+		return nil, xerrors.Errorf("scan error: %w", err)
 	}
 
-	for k, v := range tmp {
+	for k, v := range uniqueLibs {
 		libs = append(libs, types.Library{
 			Name:    k,
 			Version: v,
