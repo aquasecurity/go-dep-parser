1cd7adbada6b56f86f734015643009bd187d42e2
Sam Lane <samuel.lane@hotmail.com>
2021-05-16 19:12:35 +0100

    clean up

--- pkg/python/parse_testcase.go
+++ pkg/python/parse_testcase.go
@@ -3,11 +3,6 @@ package python
 import "github.com/aquasecurity/go-dep-parser/pkg/types"
 
 var (
-	// docker run --name pipenv --rm -it python:3.7-alpine bash
-	// mkdir app && cd /app
-	// pip install pipenv
-	// pipenv install requests pyyaml
-	// pipenv graph --json | jq -rc '.[] | "{\"\(.package.package_name | ascii_downcase)\", \"\(.package.installed_version)\"},"'
 	requirementsFlask = []types.Library{
 		{"click", "8.0.0"},
 		{"Flask", "2.0.0"},
