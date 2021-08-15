74a315d36c78fbf3a107a823d63630d33fafd66e
Ankush K <akhobragade42@gmail.com>
2021-08-15 19:59:02 +0530

    updated for working with go 1.12 env

--- pkg/php/wordpress/parse.go
+++ pkg/php/wordpress/parse.go
@@ -26,7 +26,7 @@ func Parse(r io.Reader) (lib types.Library, err error) {
 	// If wordpress file, open file and
 	// find line with content
 	// $wp_version = '<WORDPRESS_VERSION>';
-	content, err := io.ReadAll(r)
+	content, err := readAll(r)
 	if err != nil {
 		return lib, xerrors.Errorf("decode error: %w", err)
 	}
@@ -44,4 +44,22 @@ func Parse(r io.Reader) (lib types.Library, err error) {
 		Version: matches[0][2],
 		License: "",
 	}, nil
+}
+
+func readAll(r io.Reader) ([]byte, error) {
+	b := make([]byte, 0, 512)
+	for {
+		if len(b) == cap(b) {
+			// Add more capacity (let append pick how much).
+			b = append(b, 0)[:len(b)]
+		}
+		n, err := r.Read(b[len(b):cap(b)])
+		b = b[:len(b)+n]
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return b, err
+		}
+	}
 }
