3b93db5bbab7e7e11c2158e62ee069c03524e7ca
Andrey Levchenko <levchenko.andrey@gmail.com>
2022-04-07 21:05:08 +0600

    feat: add parent dependencies

--- pkg/nuget/lock/parse_testcase.go
+++ pkg/nuget/lock/parse_testcase.go
@@ -12,8 +12,8 @@ var (
 	// dotnet restore --use-lock-file
 	// cat packages.lock.json | jq -rc '.dependencies[] | keys[] as $k | "{\"\($k)\", \"\(.[$k] | .resolved)\", \"\"},"'
 	nuGetSimple = []types.Library{
-		{"Newtonsoft.Json", "12.0.3", ""},
-		{"NuGet.Frameworks", "5.7.0", ""},
+		types.NewLibrary("Newtonsoft.Json", "12.0.3", ""),
+		types.NewLibrary("NuGet.Frameworks", "5.7.0", ""),
 	}
 
 	// docker run --rm -i -t mcr.microsoft.com/dotnet/sdk:latest
@@ -25,14 +25,14 @@ var (
 	// dotnet restore --use-lock-file
 	// cat packages.lock.json | jq -rc '.dependencies[] | keys[] as $k | "{\"\($k)\", \"\(.[$k] | .resolved)\", \"\"},"'
 	nuGetSubDependencies = []types.Library{
-		{"Microsoft.Extensions.ApiDescription.Server", "3.0.0", ""},
-		{"Microsoft.OpenApi", "1.1.4", ""},
-		{"Newtonsoft.Json", "12.0.3", ""},
-		{"NuGet.Frameworks", "5.7.0", ""},
-		{"Swashbuckle.AspNetCore", "5.5.1", ""},
-		{"Swashbuckle.AspNetCore.Swagger", "5.5.1", ""},
-		{"Swashbuckle.AspNetCore.SwaggerGen", "5.5.1", ""},
-		{"Swashbuckle.AspNetCore.SwaggerUI", "5.5.1", ""},
+		types.NewLibrary("Microsoft.Extensions.ApiDescription.Server", "3.0.0", ""),
+		types.NewLibrary("Microsoft.OpenApi", "1.1.4", ""),
+		types.NewLibrary("Newtonsoft.Json", "12.0.3", ""),
+		types.NewLibrary("NuGet.Frameworks", "5.7.0", ""),
+		types.NewLibrary("Swashbuckle.AspNetCore", "5.5.1", ""),
+		types.NewLibrary("Swashbuckle.AspNetCore.Swagger", "5.5.1", ""),
+		types.NewLibrary("Swashbuckle.AspNetCore.SwaggerGen", "5.5.1", ""),
+		types.NewLibrary("Swashbuckle.AspNetCore.SwaggerUI", "5.5.1", ""),
 	}
 
 	// mcr.microsoft.com/dotnet/sdk:latest
@@ -44,8 +44,8 @@ var (
 	// dotnet restore --use-lock-file
 	// cat packages.lock.json | jq -rc '.dependencies[] | keys[] as $k | "{\"\($k)\", \"\(.[$k] | .resolved)\", \"\"},"'
 	nuGetLegacy = []types.Library{
-		{"AWSSDK.Core", "3.5.1.30", ""},
-		{"Newtonsoft.Json", "12.0.3", ""},
+		types.NewLibrary("AWSSDK.Core", "3.5.1.30", ""),
+		types.NewLibrary("Newtonsoft.Json", "12.0.3", ""),
 	}
 
 	// docker run --rm -i -t mcr.microsoft.com/dotnet/sdk:latest
@@ -58,45 +58,45 @@ var (
 	// dotnet add package AWSSDK.Core
 	// cat packages.lock.json | jq -rc '.dependencies[] | keys[] as $k | "{\"\($k)\", \"\(.[$k] | .resolved)\", \"\"},"' | sort -u
 	nuGetMultiTarget = []types.Library{
-		{"AWSSDK.Core", "3.5.1.30", ""},
-		{"Microsoft.Bcl.AsyncInterfaces", "1.1.0", ""},
-		{"Microsoft.CSharp", "4.3.0", ""},
-		{"Microsoft.NETCore.Platforms", "1.1.0", ""},
-		{"Microsoft.NETCore.Targets", "1.1.0", ""},
-		{"Microsoft.NETFramework.ReferenceAssemblies", "1.0.0", ""},
-		{"Microsoft.NETFramework.ReferenceAssemblies.net20", "1.0.0", ""},
-		{"Microsoft.NETFramework.ReferenceAssemblies.net40", "1.0.0", ""},
-		{"NETStandard.Library", "1.6.1", ""},
-		{"NETStandard.Library", "2.0.3", ""},
-		{"Newtonsoft.Json", "12.0.3", ""},
-		{"System.Collections", "4.3.0", ""},
-		{"System.ComponentModel", "4.3.0", ""},
-		{"System.ComponentModel.Primitives", "4.3.0", ""},
-		{"System.ComponentModel.TypeConverter", "4.3.0", ""},
-		{"System.Diagnostics.Debug", "4.3.0", ""},
-		{"System.Diagnostics.Tools", "4.3.0", ""},
-		{"System.Dynamic.Runtime", "4.3.0", ""},
-		{"System.Globalization", "4.3.0", ""},
-		{"System.IO", "4.3.0", ""},
-		{"System.Linq", "4.3.0", ""},
-		{"System.Linq.Expressions", "4.3.0", ""},
-		{"System.Net.Primitives", "4.3.0", ""},
-		{"System.ObjectModel", "4.3.0", ""},
-		{"System.Reflection", "4.3.0", ""},
-		{"System.Reflection.Extensions", "4.3.0", ""},
-		{"System.Reflection.Primitives", "4.3.0", ""},
-		{"System.Resources.ResourceManager", "4.3.0", ""},
-		{"System.Runtime", "4.3.0", ""},
-		{"System.Runtime.CompilerServices.Unsafe", "4.5.2", ""},
-		{"System.Runtime.Extensions", "4.3.0", ""},
-		{"System.Runtime.Serialization.Primitives", "4.3.0", ""},
-		{"System.Text.Encoding", "4.3.0", ""},
-		{"System.Text.Encoding.Extensions", "4.3.0", ""},
-		{"System.Text.RegularExpressions", "4.3.0", ""},
-		{"System.Threading", "4.3.0", ""},
-		{"System.Threading.Tasks", "4.3.0", ""},
-		{"System.Threading.Tasks.Extensions", "4.5.2", ""},
-		{"System.Xml.ReaderWriter", "4.3.0", ""},
-		{"System.Xml.XDocument", "4.3.0", ""},
+		types.NewLibrary("AWSSDK.Core", "3.5.1.30", ""),
+		types.NewLibrary("Microsoft.Bcl.AsyncInterfaces", "1.1.0", ""),
+		types.NewLibrary("Microsoft.CSharp", "4.3.0", ""),
+		types.NewLibrary("Microsoft.NETCore.Platforms", "1.1.0", ""),
+		types.NewLibrary("Microsoft.NETCore.Targets", "1.1.0", ""),
+		types.NewLibrary("Microsoft.NETFramework.ReferenceAssemblies", "1.0.0", ""),
+		types.NewLibrary("Microsoft.NETFramework.ReferenceAssemblies.net20", "1.0.0", ""),
+		types.NewLibrary("Microsoft.NETFramework.ReferenceAssemblies.net40", "1.0.0", ""),
+		types.NewLibrary("NETStandard.Library", "1.6.1", ""),
+		types.NewLibrary("NETStandard.Library", "2.0.3", ""),
+		types.NewLibrary("Newtonsoft.Json", "12.0.3", ""),
+		types.NewLibrary("System.Collections", "4.3.0", ""),
+		types.NewLibrary("System.ComponentModel", "4.3.0", ""),
+		types.NewLibrary("System.ComponentModel.Primitives", "4.3.0", ""),
+		types.NewLibrary("System.ComponentModel.TypeConverter", "4.3.0", ""),
+		types.NewLibrary("System.Diagnostics.Debug", "4.3.0", ""),
+		types.NewLibrary("System.Diagnostics.Tools", "4.3.0", ""),
+		types.NewLibrary("System.Dynamic.Runtime", "4.3.0", ""),
+		types.NewLibrary("System.Globalization", "4.3.0", ""),
+		types.NewLibrary("System.IO", "4.3.0", ""),
+		types.NewLibrary("System.Linq", "4.3.0", ""),
+		types.NewLibrary("System.Linq.Expressions", "4.3.0", ""),
+		types.NewLibrary("System.Net.Primitives", "4.3.0", ""),
+		types.NewLibrary("System.ObjectModel", "4.3.0", ""),
+		types.NewLibrary("System.Reflection", "4.3.0", ""),
+		types.NewLibrary("System.Reflection.Extensions", "4.3.0", ""),
+		types.NewLibrary("System.Reflection.Primitives", "4.3.0", ""),
+		types.NewLibrary("System.Resources.ResourceManager", "4.3.0", ""),
+		types.NewLibrary("System.Runtime", "4.3.0", ""),
+		types.NewLibrary("System.Runtime.CompilerServices.Unsafe", "4.5.2", ""),
+		types.NewLibrary("System.Runtime.Extensions", "4.3.0", ""),
+		types.NewLibrary("System.Runtime.Serialization.Primitives", "4.3.0", ""),
+		types.NewLibrary("System.Text.Encoding", "4.3.0", ""),
+		types.NewLibrary("System.Text.Encoding.Extensions", "4.3.0", ""),
+		types.NewLibrary("System.Text.RegularExpressions", "4.3.0", ""),
+		types.NewLibrary("System.Threading", "4.3.0", ""),
+		types.NewLibrary("System.Threading.Tasks", "4.3.0", ""),
+		types.NewLibrary("System.Threading.Tasks.Extensions", "4.5.2", ""),
+		types.NewLibrary("System.Xml.ReaderWriter", "4.3.0", ""),
+		types.NewLibrary("System.Xml.XDocument", "4.3.0", ""),
 	}
 )
