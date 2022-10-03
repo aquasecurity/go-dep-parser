cca727d39e63c83cd271132a5b568d0fdc249063
DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>
2022-10-03 13:52:05 +0600

    refactor
    Co-authored-by: Teppei Fukuda <knqyf263@gmail.com>

--- pkg/java/jar/parse.go
+++ pkg/java/jar/parse.go
@@ -148,7 +148,7 @@ func (p *Parser) parseArtifact(fileName string, size int64, r dio.ReadSeekerAt) 
 		case isArtifact(fileInJar.Name):
 			innerLibs, _, err := p.parseInnerJar(fileInJar) //TODO process inner deps
 			if err != nil {
-				log.Logger.Debugf("failed to parse %s: %s", fileInJar.Name, err)
+				log.Logger.Debugf("Failed to parse %s: %s", fileInJar.Name, err)
 				continue
 			}
 			libs = append(libs, innerLibs...)
