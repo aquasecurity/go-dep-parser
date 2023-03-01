7a38c0b290526130123130dad9ad905a1962d71b
Andrey Levchenko <levchenko.andrey@gmail.com>
2023-03-01 18:45:24 +0600

    feat(rust): dependency tree and line numbers support for cargo lock file

--- pkg/rust/cargo/parse_test.go
+++ pkg/rust/cargo/parse_test.go
@@ -1,6 +1,7 @@
 package cargo
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"sort"
@@ -33,6 +34,12 @@ var (
 			DependsOn: []string{"idna@0.1.5", "matches@0.1.8", "percent-encoding@1.0.1"},
 		},
 	}
+	cargoMixedLibs = []types.Library{
+		{ID: "normal@0.1.0", Name: "normal", Version: "0.1.0", Locations: []types.Location{{StartLine: 10, EndLine: 15}}},
+		{ID: "libc@0.2.54", Name: "libc", Version: "0.2.54", Locations: []types.Location{{StartLine: 3, EndLine: 6}}},
+		{ID: "typemap@0.3.3", Name: "typemap", Version: "0.3.3", Locations: []types.Location{{StartLine: 32, EndLine: 38}}},
+		{ID: "url@1.7.2", Name: "url", Version: "1.7.2", Locations: []types.Location{{StartLine: 19, EndLine: 27}}},
+	}
 )
 
 func TestParse(t *testing.T) {
@@ -40,11 +47,23 @@ func TestParse(t *testing.T) {
 		file     string // Test input file
 		wantLibs []types.Library
 		wantDeps []types.Dependency
+		wantErr  assert.ErrorAssertionFunc
 	}{
 		{
 			file:     "testdata/cargo_normal.lock",
 			wantLibs: cargoNormalLibs,
 			wantDeps: cargoNormalDeps,
+			wantErr:  assert.NoError,
+		},
+		{
+			file:     "testdata/cargo_mixed.lock",
+			wantLibs: cargoMixedLibs,
+			wantDeps: cargoNormalDeps,
+			wantErr:  assert.NoError,
+		},
+		{
+			file:    "testdata/cargo_invalid.lock",
+			wantErr: assert.Error,
 		},
 	}
 
@@ -55,27 +74,38 @@ func TestParse(t *testing.T) {
 
 			libs, deps, err := NewParser().Parse(f)
 
-			require.NoError(t, err)
+			if !v.wantErr(t, err, fmt.Sprintf("Parse(%v)", v.file)) {
+				return
+			}
 
-			sort.Slice(libs, func(i, j int) bool {
-				ret := strings.Compare(libs[i].Name, libs[j].Name)
-				if ret == 0 {
-					return libs[i].Version < libs[j].Version
-				}
-				return ret < 0
-			})
+			if err != nil {
+				return
+			}
 
-			sort.Slice(v.wantLibs, func(i, j int) bool {
-				ret := strings.Compare(v.wantLibs[i].Name, v.wantLibs[j].Name)
-				if ret == 0 {
-					return v.wantLibs[i].Version < v.wantLibs[j].Version
-				}
-				return ret < 0
-			})
+			sortLibs(libs)
+			sortLibs(v.wantLibs)
 
-			assert.Equal(t, v.wantLibs, libs)
+			sortDeps(deps)
+			sortDeps(v.wantDeps)
 
-			assert.Equal(t, v.wantDeps, deps)
+			assert.Equalf(t, v.wantLibs, libs, "Parse libraries(%v)", v.file)
+			assert.Equalf(t, v.wantDeps, deps, "Parse dependencies(%v)", v.file)
 		})
 	}
+}
+
+func sortLibs(libs []types.Library) {
+	sort.Slice(libs, func(i, j int) bool {
+		ret := strings.Compare(libs[i].Name, libs[j].Name)
+		if ret == 0 {
+			return libs[i].Version < libs[j].Version
+		}
+		return ret < 0
+	})
+}
+
+func sortDeps(deps []types.Dependency) {
+	sort.Slice(deps, func(i, j int) bool {
+		return strings.Compare(deps[i].ID, deps[j].ID) < 0
+	})
 }
