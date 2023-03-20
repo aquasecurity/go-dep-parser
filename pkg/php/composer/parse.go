361417289f87295d15cdc36f83fc9776d25bc4f3
DmitriyLewen <dmitriy.lewen@smartforce.io>
2023-03-20 13:16:32 +0600

    add support of location, licenses and graph for composer.lock files

--- pkg/php/composer/parse.go
+++ pkg/php/composer/parse.go
@@ -1,7 +1,12 @@
 package composer
 
 import (
-	"encoding/json"
+	"github.com/aquasecurity/go-dep-parser/pkg/log"
+	"github.com/aquasecurity/go-dep-parser/pkg/utils"
+	"github.com/liamg/jfather"
+	"golang.org/x/exp/maps"
+	"io"
+	"strings"
 
 	dio "github.com/aquasecurity/go-dep-parser/pkg/io"
 	"github.com/aquasecurity/go-dep-parser/pkg/types"
@@ -9,11 +14,15 @@ import (
 )
 
 type lockFile struct {
-	Packages []packageInfo
+	Packages []packageInfo `json:"packages"`
 }
 type packageInfo struct {
-	Name    string
-	Version string
+	Name      string            `json:"name"`
+	Version   string            `json:"version"`
+	Require   map[string]string `json:"require"`
+	License   []string          `json:"license"`
+	StartLine int
+	EndLine   int
 }
 
 type Parser struct{}
@@ -24,18 +33,63 @@ func NewParser() types.Parser {
 
 func (p *Parser) Parse(r dio.ReadSeekerAt) ([]types.Library, []types.Dependency, error) {
 	var lockFile lockFile
-	decoder := json.NewDecoder(r)
-	err := decoder.Decode(&lockFile)
+	input, err := io.ReadAll(r)
 	if err != nil {
+		return nil, nil, xerrors.Errorf("read error: %w", err)
+	}
+	if err = jfather.Unmarshal(input, &lockFile); err != nil {
 		return nil, nil, xerrors.Errorf("decode error: %w", err)
 	}
 
-	var libs []types.Library
+	libs := map[string]types.Library{}
+	var deps []types.Dependency
 	for _, pkg := range lockFile.Packages {
-		libs = append(libs, types.Library{
-			Name:    pkg.Name,
-			Version: pkg.Version,
-		})
+		lib := types.Library{
+			ID:        utils.PackageID(pkg.Name, pkg.Version),
+			Name:      pkg.Name,
+			Version:   pkg.Version,
+			Indirect:  true, // composer.lock file doesn't have info about Direct/Indirect deps.
+			License:   strings.Join(pkg.License, ", "),
+			Locations: []types.Location{{StartLine: pkg.StartLine, EndLine: pkg.EndLine}},
+		}
+		libs[lib.Name] = lib
+
+		var dependsOn []string
+		for depName := range pkg.Require {
+			if depName != "php" { // Require field includes required php version, skip this
+				dependsOn = append(dependsOn, depName) // field uses range of versions, so later we will fill in the versions from the libraries
+			}
+		}
+		if len(dependsOn) > 0 {
+			dep := types.Dependency{
+				ID:        lib.ID,
+				DependsOn: dependsOn,
+			}
+			deps = append(deps, dep)
+		}
 	}
-	return libs, nil, nil
+
+	// fill deps versions
+	for i, dep := range deps {
+		for j, depName := range dep.DependsOn {
+			if lib, ok := libs[depName]; ok {
+				deps[i].DependsOn[j] = lib.ID
+				continue
+			}
+			log.Logger.Debugf("unable to find version of %s", depName)
+		}
+	}
+
+	return maps.Values(libs), deps, nil
+}
+
+// UnmarshalJSONWithMetadata needed to detect start and end lines of deps
+func (t *packageInfo) UnmarshalJSONWithMetadata(node jfather.Node) error {
+	if err := node.Decode(&t); err != nil {
+		return err
+	}
+	// Decode func will overwrite line numbers if we save them first
+	t.StartLine = node.Range().Start.Line
+	t.EndLine = node.Range().End.Line
+	return nil
 }
--- pkg/php/composer/parse_test.go
+++ pkg/php/composer/parse_test.go
@@ -1,48 +1,88 @@
 package composer
 
 import (
-	"os"
-	"path"
-	"testing"
-
+	"github.com/aquasecurity/go-dep-parser/pkg/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"os"
+	"sort"
+	"strings"
+	"testing"
+)
 
-	"github.com/aquasecurity/go-dep-parser/pkg/types"
+var (
+	// docker run --name composer --rm -it composer@sha256:082ed124b68e7e880721772a6bf22ad809e3bc87db8bbee9f0ec7127bb21ccad bash
+	// apk add jq
+	// composer require laravel/installer
+	// composer require pear/log --dev
+	// composer show -i --no-dev -f json | jq --sort-keys -rc '.installed[] | "{ID: \"\(.name)@\(.version)\", Name: \"\(.name)\", Version: \"\(.version)\", Indirect: true, License: \"\", Locations: []types.Location{{StartLine: , EndLine: }}},"'
+	// locations are filled manually
+	composerLibs = []types.Library{
+		{ID: "laravel/installer@v4.4.3", Name: "laravel/installer", Version: "v4.4.3", Indirect: true, License: "MIT", Locations: []types.Location{{StartLine: 9, EndLine: 65}}},
+		{ID: "psr/container@2.0.2", Name: "psr/container", Version: "2.0.2", Indirect: true, License: "MIT", Locations: []types.Location{{StartLine: 66, EndLine: 118}}},
+		{ID: "symfony/console@v6.2.7", Name: "symfony/console", Version: "v6.2.7", Indirect: true, License: "MIT", Locations: []types.Location{{StartLine: 119, EndLine: 214}}},
+		{ID: "symfony/deprecation-contracts@v3.2.1", Name: "symfony/deprecation-contracts", Version: "v3.2.1", Indirect: true, License: "MIT", Locations: []types.Location{{StartLine: 215, EndLine: 281}}},
+		{ID: "symfony/polyfill-ctype@v1.27.0", Name: "symfony/polyfill-ctype", Version: "v1.27.0", Indirect: true, License: "MIT", Locations: []types.Location{{StartLine: 282, EndLine: 363}}},
+		{ID: "symfony/polyfill-intl-grapheme@v1.27.0", Name: "symfony/polyfill-intl-grapheme", Version: "v1.27.0", Indirect: true, License: "MIT", Locations: []types.Location{{StartLine: 364, EndLine: 444}}},
+		{ID: "symfony/polyfill-intl-normalizer@v1.27.0", Name: "symfony/polyfill-intl-normalizer", Version: "v1.27.0", Indirect: true, License: "MIT", Locations: []types.Location{{StartLine: 445, EndLine: 528}}},
+		{ID: "symfony/polyfill-mbstring@v1.27.0", Name: "symfony/polyfill-mbstring", Version: "v1.27.0", Indirect: true, License: "MIT", Locations: []types.Location{{StartLine: 529, EndLine: 611}}},
+		{ID: "symfony/process@v6.2.7", Name: "symfony/process", Version: "v6.2.7", Indirect: true, License: "MIT", Locations: []types.Location{{StartLine: 612, EndLine: 672}}},
+		{ID: "symfony/service-contracts@v3.2.1", Name: "symfony/service-contracts", Version: "v3.2.1", Indirect: true, License: "MIT", Locations: []types.Location{{StartLine: 673, EndLine: 757}}},
+		{ID: "symfony/string@v6.2.7", Name: "symfony/string", Version: "v6.2.7", Indirect: true, License: "MIT", Locations: []types.Location{{StartLine: 758, EndLine: 843}}},
+	}
+	// dependencies are filled manually
+	composerDeps = []types.Dependency{
+		{ID: "laravel/installer@v4.4.3", DependsOn: []string{"symfony/console@v6.2.7", "symfony/process@v6.2.7"}},
+		{ID: "symfony/console@v6.2.7", DependsOn: []string{"symfony/deprecation-contracts@v3.2.1", "symfony/polyfill-mbstring@v1.27.0", "symfony/service-contracts@v3.2.1", "symfony/string@v6.2.7"}},
+		{ID: "symfony/service-contracts@v3.2.1", DependsOn: []string{"psr/container@2.0.2"}},
+		{ID: "symfony/string@v6.2.7", DependsOn: []string{"symfony/polyfill-ctype@v1.27.0", "symfony/polyfill-intl-grapheme@v1.27.0", "symfony/polyfill-intl-normalizer@v1.27.0", "symfony/polyfill-mbstring@v1.27.0"}},
+	}
 )
 
 func TestParse(t *testing.T) {
-	vectors := []struct {
-		file string // Test input file
-		want []types.Library
+	tests := []struct {
+		name     string
+		file     string
+		wantLibs []types.Library
+		wantDeps []types.Dependency
 	}{
 		{
-			file: "testdata/composer_normal.lock",
-			want: ComposerNormal,
-		},
-		{
-			file: "testdata/composer_laravel.lock",
-			want: ComposerLaravel,
-		},
-		{
-			file: "testdata/composer_symfony.lock",
-			want: ComposerSymfony,
-		},
-		{
-			file: "testdata/composer_with_dev.lock",
-			want: ComposerWithDev,
+			name:     "happy path",
+			file:     "testdata/composer_happy.lock",
+			wantLibs: composerLibs,
+			wantDeps: composerDeps,
 		},
 	}
 
-	for _, v := range vectors {
-		t.Run(path.Base(v.file), func(t *testing.T) {
-			f, err := os.Open(v.file)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f, err := os.Open(tt.file)
 			require.NoError(t, err)
 
-			got, _, err := NewParser().Parse(f)
+			got, deps, err := NewParser().Parse(f)
 			require.NoError(t, err)
 
-			assert.Equal(t, v.want, got)
+			sortLibs(got)
+			sortDeps(deps)
+
+			assert.Equal(t, tt.wantLibs, got)
+			assert.Equal(t, tt.wantDeps, deps)
 		})
 	}
+}
+
+func sortDeps(deps []types.Dependency) {
+	sort.Slice(deps, func(i, j int) bool {
+		return strings.Compare(deps[i].ID, deps[j].ID) < 0
+	})
+
+	for i := range deps {
+		sort.Strings(deps[i].DependsOn)
+	}
+}
+
+func sortLibs(libs []types.Library) {
+	sort.Slice(libs, func(i, j int) bool {
+		return strings.Compare(libs[i].ID, libs[j].ID) < 0
+	})
 }
