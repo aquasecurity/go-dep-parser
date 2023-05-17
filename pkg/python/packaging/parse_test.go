c41fb69acaad422c451450afcf39e170b52b67b2
afdesk <work@afdesk.com>
2023-05-17 12:10:13 +0600

    add test-cases

--- pkg/python/packaging/parse_test.go
+++ pkg/python/packaging/parse_test.go
@@ -72,6 +72,30 @@ func TestParse(t *testing.T) {
 			input:   "testdata/invalid.json",
 			wantErr: true,
 		},
+		{
+			name:  "with License-Expression field",
+			input: "testdata/iniconfig-2.0.0.METADATA",
+			want: []types.Library{
+				{
+					Name:        "iniconfig",
+					Version:     "2.0.0",
+					License:     "MIT",
+					LicenseFile: "LICENSE",
+				},
+			},
+		},
+		{
+			name:  "with an empty license field but with license in Classifier",
+			input: "testdata/zipp-3.12.1.METADATA",
+			want: []types.Library{
+				{
+					Name:        "zipp",
+					Version:     "3.12.1",
+					License:     "MIT License",
+					LicenseFile: "LICENSE",
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
