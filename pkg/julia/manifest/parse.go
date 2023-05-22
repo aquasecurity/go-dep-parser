cdd683f790d6cbc2992283b3f4ddb6d21fda4d41
Octogonapus <firey45@gmail.com>
2023-05-22 19:23:56 -0400

    Export manifest and dep

--- pkg/julia/manifest/parse.go
+++ pkg/julia/manifest/parse.go
@@ -12,15 +12,15 @@ import (
 	"golang.org/x/xerrors"
 )
 
-type manifest struct {
-	JuliaVersion   string           `toml:"julia_version"`
-	ManifestFormat string           `toml:"manifest_format"`
-	Deps           map[string][]dep `toml:"deps"` // e.g. [[deps.Foo]]
+type Manifest struct {
+	JuliaVersion   string                  `toml:"julia_version"`
+	ManifestFormat string                  `toml:"manifest_format"`
+	Dependencies   map[string][]Dependency `toml:"deps"` // e.g. [[deps.Foo]]
 }
-type dep struct {
-	Deps    []string `toml:"deps"` // by name. e.g. deps = ["Foo"]
-	UUID    string   `toml:"uuid"`
-	Version string   `toml:"version"` // not specified for stdlib packages, which are of the Julia version
+type Dependency struct {
+	Dependencies []string `toml:"deps"` // by name. e.g. deps = ["Foo"]
+	UUID         string   `toml:"uuid"`
+	Version      string   `toml:"version"` // not specified for stdlib packages, which are of the Julia version
 }
 
 type Parser struct{}
@@ -30,8 +30,8 @@ func NewParser() types.Parser {
 }
 
 func (p *Parser) Parse(r dio.ReadSeekerAt) ([]types.Library, []types.Dependency, error) {
-	var oldDeps map[string][]dep
-	var man manifest
+	var oldDeps map[string][]Dependency
+	var man Manifest
 	decoder := toml.NewDecoder(r)
 	// Try to read the old Manifest format. If that fails, try the new format.
 	if _, err := decoder.Decode(&oldDeps); err != nil {
@@ -44,10 +44,10 @@ func (p *Parser) Parse(r dio.ReadSeekerAt) ([]types.Library, []types.Dependency,
 	// We can't know the Julia version on an old manifest.
 	// All newer manifests include a manifest version and a julia version.
 	if man.ManifestFormat == "" {
-		man = manifest{
+		man = Manifest{
 			JuliaVersion:   "unknown",
 			ManifestFormat: "unknown",
-			Deps:           oldDeps,
+			Dependencies:   oldDeps,
 		}
 	}
 
@@ -61,7 +61,7 @@ func (p *Parser) Parse(r dio.ReadSeekerAt) ([]types.Library, []types.Dependency,
 
 	var libs []types.Library
 	var deps []types.Dependency
-	for name, manifestDeps := range man.Deps {
+	for name, manifestDeps := range man.Dependencies {
 		if len(manifestDeps) > 1 {
 			return nil, nil, xerrors.Errorf("multiple entries for dep: %s", name)
 		}
@@ -78,7 +78,7 @@ func (p *Parser) Parse(r dio.ReadSeekerAt) ([]types.Library, []types.Dependency,
 		}
 
 		libs = append(libs, lib)
-		dep := parseDependencies(pkgID, manifestDep, man.Deps, man.JuliaVersion)
+		dep := parseDependencies(pkgID, manifestDep, man.Dependencies, man.JuliaVersion)
 		if dep != nil {
 			deps = append(deps, *dep)
 		}
@@ -89,10 +89,10 @@ func (p *Parser) Parse(r dio.ReadSeekerAt) ([]types.Library, []types.Dependency,
 }
 
 // Returns the dependencies in `deps` of the given `dep`. If there are no dependencies, returns `nil`.
-func parseDependencies(pkgId string, d dep, deps map[string][]dep, juliaVersion string) *types.Dependency {
+func parseDependencies(pkgId string, dep Dependency, deps map[string][]Dependency, juliaVersion string) *types.Dependency {
 	var dependOn []string
 
-	for _, pkgDep := range d.Deps {
+	for _, pkgDep := range dep.Dependencies {
 		dep := deps[pkgDep][0]
 		version := depVersion(dep, juliaVersion)
 		dependOn = append(dependOn, utils.PackageID(dep.UUID, version))
@@ -110,9 +110,9 @@ func parseDependencies(pkgId string, d dep, deps map[string][]dep, juliaVersion 
 
 // Returns the effective version of the `dep`.
 // stdlib packages do not have a version in the manifest because they are packaged with julia itself
-func depVersion(d dep, juliaVersion string) string {
-	if len(d.Version) == 0 {
+func depVersion(dep Dependency, juliaVersion string) string {
+	if len(dep.Version) == 0 {
 		return juliaVersion
 	}
-	return d.Version
+	return dep.Version
 }
