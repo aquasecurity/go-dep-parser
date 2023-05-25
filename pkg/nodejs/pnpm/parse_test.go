62f516542ada8c910a3ca688807419ac0a3eeb5b
DmitriyLewen <dmitriy.lewen@smartforce.io>
2023-05-25 11:03:51 +0600

    add test for long registry name

--- pkg/nodejs/pnpm/parse_test.go
+++ pkg/nodejs/pnpm/parse_test.go
@@ -103,6 +103,20 @@ func TestGetPackageNameAndVersion(t *testing.T) {
 			wantVersion: "4.17.10",
 		},
 		{
+			name:        "v5 - registry",
+			lockFileVer: 5.0,
+			pkg:         "registry.npmjs.org/test/lodash/4.17.10",
+			wantName:    "lodash",
+			wantVersion: "4.17.10",
+		},
+		{
+			name:        "v5 - long registry name",
+			lockFileVer: 5.0,
+			pkg:         "repo.fury.io/account/lodash/4.17.10",
+			wantName:    "lodash",
+			wantVersion: "4.17.10",
+		},
+		{
 			name:        "v5 - relative path with slash",
 			lockFileVer: 5.0,
 			pkg:         "/@babel/generator/7.21.9",
