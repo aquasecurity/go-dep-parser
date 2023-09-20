0477cf9c0b1996a684efe80a01c8d8cbecbffbd9
Benji Visser <benji@093b.org>
2023-09-20 15:10:28 -0700

    add dotnet deps parsing
    Signed-off-by: Benji Visser <benji@093b.org>

--- pkg/dotnet/core_deps/parse.go
+++ pkg/dotnet/core_deps/parse.go
@@ -1,14 +1,17 @@
 package core_deps
 
 import (
-	"github.com/liamg/jfather"
+	"fmt"
 	"io"
 	"strings"
+
+	"github.com/liamg/jfather"
 
 	"golang.org/x/xerrors"
 
 	dio "github.com/aquasecurity/go-dep-parser/pkg/io"
 	"github.com/aquasecurity/go-dep-parser/pkg/types"
+	"github.com/aquasecurity/go-dep-parser/pkg/utils"
 
 	"github.com/aquasecurity/go-dep-parser/pkg/log"
 )
@@ -17,6 +20,22 @@ type Parser struct{}
 
 func NewParser() types.Parser {
 	return &Parser{}
+}
+
+func splitNameVer(nameVer string) (name, version string) {
+	split := strings.Split(nameVer, "/")
+	if len(split) != 2 {
+		// Invalid name
+		log.Logger.Warnf("Cannot parse .NET library version from: %s", nameVer)
+		return "", ""
+	}
+	name = split[0]
+	version = split[1]
+	return
+}
+
+func joinNameVer(name, version string) (nameVer string) {
+	return fmt.Sprintf("%s/%s", name, version)
 }
 
 func (p *Parser) Parse(r dio.ReadSeekerAt) ([]types.Library, []types.Dependency, error) {
@@ -31,30 +50,60 @@ func (p *Parser) Parse(r dio.ReadSeekerAt) ([]types.Library, []types.Dependency,
 	}
 
 	var libraries []types.Library
-	for nameVer, lib := range depsFile.Libraries {
-		if !strings.EqualFold(lib.Type, "package") {
+	var deps []types.Dependency
+	for pkgNameVersion, target := range depsFile.Targets[depsFile.RuntimeTarget.Name] {
+		library, ok := depsFile.Libraries[pkgNameVersion]
+		if !ok {
 			continue
 		}
 
-		split := strings.Split(nameVer, "/")
-		if len(split) != 2 {
-			// Invalid name
-			log.Logger.Warnf("Cannot parse .NET library version from: %s", nameVer)
+		name, version := splitNameVer(pkgNameVersion)
+		if name == "" || version == "" {
 			continue
 		}
 
-		libraries = append(libraries, types.Library{
-			Name:      split[0],
-			Version:   split[1],
-			Locations: []types.Location{{StartLine: lib.StartLine, EndLine: lib.EndLine}},
+		lib := types.Library{
+			ID:        utils.PackageID(name, version),
+			Name:      name,
+			Version:   version,
+			Locations: []types.Location{{StartLine: library.StartLine, EndLine: library.EndLine}},
+		}
+
+		var childDeps []string
+		for depName, depVersion := range target.Dependencies {
+			nameVer := joinNameVer(depName, depVersion)
+			_, ok := depsFile.Libraries[nameVer]
+			if !ok {
+				continue
+			}
+			depID := utils.PackageID(depName, depVersion)
+			childDeps = append(childDeps, depID)
+		}
+
+		deps = append(deps, types.Dependency{
+			ID:        lib.ID,
+			DependsOn: childDeps,
 		})
+
+		libraries = append(libraries, lib)
 	}
 
-	return libraries, nil, nil
+	return libraries, deps, nil
 }
 
 type dotNetDependencies struct {
-	Libraries map[string]dotNetLibrary `json:"libraries"`
+	RuntimeTarget dotNetRuntimeTarget                `json:"runtimeTarget"`
+	Libraries     map[string]dotNetLibrary           `json:"libraries"`
+	Targets       map[string]map[string]dotNetTarget `json:"targets"`
+}
+
+type dotNetRuntimeTarget struct {
+	Name string `json:"name"`
+}
+
+type dotNetTarget struct {
+	Dependencies map[string]string   `json:"dependencies"`
+	Runtime      map[string]struct{} `json:"runtime"`
 }
 
 type dotNetLibrary struct {
--- pkg/dotnet/core_deps/parse_test.go
+++ pkg/dotnet/core_deps/parse_test.go
@@ -15,19 +15,251 @@ import (
 
 func TestParse(t *testing.T) {
 	vectors := []struct {
-		file    string // Test input file
-		want    []types.Library
-		wantErr string
+		file     string // Test input file
+		wantLibs []types.Library
+		wantDeps []types.Dependency
+		wantErr  string
 	}{
 		{
-			file: "testdata/ExampleApp1.deps.json",
-			want: []types.Library{
-				{Name: "Newtonsoft.Json", Version: "13.0.1", Locations: []types.Location{{StartLine: 33, EndLine: 39}}},
+			file: "testdata/MyExample.deps.json",
+			wantLibs: []types.Library{
+				{ID: "MyWebApp@1.0.0", Name: "MyWebApp", Version: "1.0.0", Locations: []types.Location{{StartLine: 148, EndLine: 152}}},
+				{ID: "Microsoft.AspNetCore.Server.IIS@2.2.6", Name: "Microsoft.AspNetCore.Server.IIS", Version: "2.2.6", Locations: []types.Location{{StartLine: 216, EndLine: 222}}},
+				{ID: "Microsoft.AspNetCore.Authentication.Abstractions@2.2.0", Name: "Microsoft.AspNetCore.Authentication.Abstractions", Version: "2.2.0", Locations: []types.Location{{StartLine: 153, EndLine: 159}}},
+				{ID: "Microsoft.AspNetCore.Authentication.Core@2.2.0", Name: "Microsoft.AspNetCore.Authentication.Core", Version: "2.2.0", Locations: []types.Location{{StartLine: 160, EndLine: 166}}},
+				{ID: "Microsoft.AspNetCore.Connections.Abstractions@2.2.0", Name: "Microsoft.AspNetCore.Connections.Abstractions", Version: "2.2.0", Locations: []types.Location{{StartLine: 167, EndLine: 173}}},
+				{ID: "Microsoft.AspNetCore.Hosting.Abstractions@2.2.0", Name: "Microsoft.AspNetCore.Hosting.Abstractions", Version: "2.2.0", Locations: []types.Location{{StartLine: 174, EndLine: 180}}},
+				{ID: "Microsoft.AspNetCore.Hosting.Server.Abstractions@2.2.0", Name: "Microsoft.AspNetCore.Hosting.Server.Abstractions", Version: "2.2.0", Locations: []types.Location{{StartLine: 181, EndLine: 187}}},
+				{ID: "Microsoft.AspNetCore.Http@2.2.0", Name: "Microsoft.AspNetCore.Http", Version: "2.2.0", Locations: []types.Location{{StartLine: 188, EndLine: 194}}},
+				{ID: "Microsoft.AspNetCore.Http.Abstractions@2.2.0", Name: "Microsoft.AspNetCore.Http.Abstractions", Version: "2.2.0", Locations: []types.Location{{StartLine: 195, EndLine: 201}}},
+				{ID: "Microsoft.AspNetCore.Http.Extensions@2.2.0", Name: "Microsoft.AspNetCore.Http.Extensions", Version: "2.2.0", Locations: []types.Location{{StartLine: 202, EndLine: 208}}},
+				{ID: "Microsoft.AspNetCore.Http.Features@2.2.0", Name: "Microsoft.AspNetCore.Http.Features", Version: "2.2.0", Locations: []types.Location{{StartLine: 209, EndLine: 215}}},
+				{ID: "Microsoft.AspNetCore.WebUtilities@2.2.0", Name: "Microsoft.AspNetCore.WebUtilities", Version: "2.2.0", Locations: []types.Location{{StartLine: 223, EndLine: 229}}},
+				{ID: "Microsoft.Extensions.Configuration.Abstractions@2.2.0", Name: "Microsoft.Extensions.Configuration.Abstractions", Version: "2.2.0", Locations: []types.Location{{StartLine: 230, EndLine: 236}}},
+				{ID: "Microsoft.Extensions.DependencyInjection.Abstractions@2.2.0", Name: "Microsoft.Extensions.DependencyInjection.Abstractions", Version: "2.2.0", Locations: []types.Location{{StartLine: 237, EndLine: 243}}},
+				{ID: "Microsoft.Extensions.FileProviders.Abstractions@2.2.0", Name: "Microsoft.Extensions.FileProviders.Abstractions", Version: "2.2.0", Locations: []types.Location{{StartLine: 244, EndLine: 250}}},
+				{ID: "Microsoft.Extensions.Hosting.Abstractions@2.2.0", Name: "Microsoft.Extensions.Hosting.Abstractions", Version: "2.2.0", Locations: []types.Location{{StartLine: 251, EndLine: 257}}},
+				{ID: "Microsoft.Extensions.Logging.Abstractions@2.2.0", Name: "Microsoft.Extensions.Logging.Abstractions", Version: "2.2.0", Locations: []types.Location{{StartLine: 258, EndLine: 264}}},
+				{ID: "Microsoft.Extensions.ObjectPool@2.2.0", Name: "Microsoft.Extensions.ObjectPool", Version: "2.2.0", Locations: []types.Location{{StartLine: 265, EndLine: 271}}},
+				{ID: "Microsoft.Extensions.Options@2.2.0", Name: "Microsoft.Extensions.Options", Version: "2.2.0", Locations: []types.Location{{StartLine: 272, EndLine: 278}}},
+				{ID: "Microsoft.Extensions.Primitives@2.2.0", Name: "Microsoft.Extensions.Primitives", Version: "2.2.0", Locations: []types.Location{{StartLine: 279, EndLine: 285}}},
+				{ID: "Microsoft.Net.Http.Headers@2.2.0", Name: "Microsoft.Net.Http.Headers", Version: "2.2.0", Locations: []types.Location{{StartLine: 286, EndLine: 292}}},
+				{ID: "Microsoft.NETCore.Platforms@2.0.0", Name: "Microsoft.NETCore.Platforms", Version: "2.0.0", Locations: []types.Location{{StartLine: 293, EndLine: 299}}},
+				{ID: "System.Buffers@4.5.0", Name: "System.Buffers", Version: "4.5.0", Locations: []types.Location{{StartLine: 300, EndLine: 306}}},
+				{ID: "System.ComponentModel.Annotations@4.5.0", Name: "System.ComponentModel.Annotations", Version: "4.5.0", Locations: []types.Location{{StartLine: 307, EndLine: 313}}},
+				{ID: "System.IO.Pipelines@4.5.3", Name: "System.IO.Pipelines", Version: "4.5.3", Locations: []types.Location{{StartLine: 314, EndLine: 320}}},
+				{ID: "System.Memory@4.5.1", Name: "System.Memory", Version: "4.5.1", Locations: []types.Location{{StartLine: 321, EndLine: 327}}},
+				{ID: "System.Runtime.CompilerServices.Unsafe@4.5.1", Name: "System.Runtime.CompilerServices.Unsafe", Version: "4.5.1", Locations: []types.Location{{StartLine: 328, EndLine: 334}}},
+				{ID: "System.Security.Principal.Windows@4.5.0", Name: "System.Security.Principal.Windows", Version: "4.5.0", Locations: []types.Location{{StartLine: 335, EndLine: 341}}},
+				{ID: "System.Text.Encodings.Web@4.5.0", Name: "System.Text.Encodings.Web", Version: "4.5.0", Locations: []types.Location{{StartLine: 342, EndLine: 348}}},
+			},
+			wantDeps: []types.Dependency{
+				{
+					ID: "MyWebApp@1.0.0",
+					DependsOn: []string{
+						"Microsoft.AspNetCore.Server.IIS@2.2.6",
+					},
+				},
+				{
+					ID: "Microsoft.AspNetCore.Authentication.Abstractions@2.2.0",
+					DependsOn: []string{
+						"Microsoft.AspNetCore.Http.Abstractions@2.2.0",
+						"Microsoft.Extensions.Logging.Abstractions@2.2.0",
+						"Microsoft.Extensions.Options@2.2.0",
+					},
+				},
+				{
+					ID: "Microsoft.AspNetCore.Http.Extensions@2.2.0",
+					DependsOn: []string{
+						"Microsoft.AspNetCore.Http.Abstractions@2.2.0",
+						"Microsoft.Extensions.FileProviders.Abstractions@2.2.0",
+						"Microsoft.Net.Http.Headers@2.2.0",
+						"System.Buffers@4.5.0",
+					},
+				},
+				{
+					ID: "Microsoft.AspNetCore.Authentication.Core@2.2.0",
+					DependsOn: []string{
+						"Microsoft.AspNetCore.Authentication.Abstractions@2.2.0",
+						"Microsoft.AspNetCore.Http@2.2.0",
+						"Microsoft.AspNetCore.Http.Extensions@2.2.0",
+					},
+				},
+				{
+					ID: "Microsoft.AspNetCore.Connections.Abstractions@2.2.0",
+					DependsOn: []string{
+						"Microsoft.AspNetCore.Http.Features@2.2.0",
+						"System.IO.Pipelines@4.5.3",
+					},
+				},
+				{
+					ID: "Microsoft.AspNetCore.Hosting.Abstractions@2.2.0",
+					DependsOn: []string{
+						"Microsoft.AspNetCore.Hosting.Server.Abstractions@2.2.0",
+						"Microsoft.AspNetCore.Http.Abstractions@2.2.0",
+						"Microsoft.Extensions.Hosting.Abstractions@2.2.0",
+					},
+				},
+				{
+					ID: "Microsoft.AspNetCore.Http.Abstractions@2.2.0",
+					DependsOn: []string{
+						"Microsoft.AspNetCore.Http.Features@2.2.0",
+						"System.Text.Encodings.Web@4.5.0",
+					},
+				},
+				{
+					ID: "Microsoft.AspNetCore.Hosting.Server.Abstractions@2.2.0",
+					DependsOn: []string{
+						"Microsoft.AspNetCore.Http.Features@2.2.0",
+						"Microsoft.Extensions.Configuration.Abstractions@2.2.0",
+					},
+				},
+				{
+					ID: "Microsoft.AspNetCore.Http@2.2.0",
+					DependsOn: []string{
+						"Microsoft.AspNetCore.Http.Abstractions@2.2.0",
+						"Microsoft.AspNetCore.WebUtilities@2.2.0",
+						"Microsoft.Extensions.ObjectPool@2.2.0",
+						"Microsoft.Extensions.Options@2.2.0",
+						"Microsoft.Net.Http.Headers@2.2.0",
+					},
+				},
+				{
+					ID: "Microsoft.AspNetCore.Http.Features@2.2.0",
+					DependsOn: []string{
+						"Microsoft.Extensions.Primitives@2.2.0",
+					},
+				},
+				{
+					ID: "Microsoft.AspNetCore.Server.IIS@2.2.6",
+					DependsOn: []string{
+						"Microsoft.AspNetCore.Authentication.Core@2.2.0",
+						"Microsoft.AspNetCore.Connections.Abstractions@2.2.0",
+						"Microsoft.AspNetCore.Hosting.Abstractions@2.2.0",
+						"System.IO.Pipelines@4.5.3",
+						"System.Security.Principal.Windows@4.5.0",
+					},
+				},
+				{
+					ID: "Microsoft.AspNetCore.WebUtilities@2.2.0",
+					DependsOn: []string{
+						"Microsoft.Net.Http.Headers@2.2.0",
+						"System.Text.Encodings.Web@4.5.0",
+					},
+				},
+				{
+					ID: "Microsoft.Extensions.Configuration.Abstractions@2.2.0",
+					DependsOn: []string{
+						"Microsoft.Extensions.Primitives@2.2.0",
+					},
+				},
+				{
+					ID:        "Microsoft.Extensions.DependencyInjection.Abstractions@2.2.0",
+					DependsOn: nil,
+				},
+				{
+					ID: "Microsoft.Extensions.FileProviders.Abstractions@2.2.0",
+					DependsOn: []string{
+						"Microsoft.Extensions.Primitives@2.2.0",
+					},
+				},
+				{
+					ID: "Microsoft.Extensions.Hosting.Abstractions@2.2.0",
+					DependsOn: []string{
+						"Microsoft.Extensions.Configuration.Abstractions@2.2.0",
+						"Microsoft.Extensions.DependencyInjection.Abstractions@2.2.0",
+						"Microsoft.Extensions.FileProviders.Abstractions@2.2.0",
+						"Microsoft.Extensions.Logging.Abstractions@2.2.0",
+					},
+				},
+				{
+					ID:        "Microsoft.Extensions.Logging.Abstractions@2.2.0",
+					DependsOn: nil,
+				},
+				{
+					ID:        "Microsoft.Extensions.ObjectPool@2.2.0",
+					DependsOn: nil,
+				},
+				{
+					ID: "Microsoft.Extensions.Options@2.2.0",
+					DependsOn: []string{
+						"Microsoft.Extensions.DependencyInjection.Abstractions@2.2.0",
+						"Microsoft.Extensions.Primitives@2.2.0",
+						"System.ComponentModel.Annotations@4.5.0",
+					},
+				},
+				{
+					ID: "Microsoft.Extensions.Primitives@2.2.0",
+					DependsOn: []string{
+						"System.Memory@4.5.1",
+						"System.Runtime.CompilerServices.Unsafe@4.5.1",
+					},
+				},
+				{
+					ID: "Microsoft.Net.Http.Headers@2.2.0",
+					DependsOn: []string{
+						"System.Buffers@4.5.0",
+						"Microsoft.Extensions.Primitives@2.2.0",
+					},
+				},
+				{
+					ID:        "Microsoft.NETCore.Platforms@2.0.0",
+					DependsOn: nil,
+				},
+				{
+					ID:        "System.Buffers@4.5.0",
+					DependsOn: nil,
+				},
+				{
+					ID:        "System.ComponentModel.Annotations@4.5.0",
+					DependsOn: nil,
+				},
+				{
+					ID:        "System.IO.Pipelines@4.5.3",
+					DependsOn: nil,
+				},
+				{
+					ID:        "System.Memory@4.5.1",
+					DependsOn: nil,
+				},
+				{
+					ID:        "System.Runtime.CompilerServices.Unsafe@4.5.1",
+					DependsOn: nil,
+				},
+				{
+					ID:        "System.Text.Encodings.Web@4.5.0",
+					DependsOn: nil,
+				},
+				{
+					ID: "System.Security.Principal.Windows@4.5.0",
+					DependsOn: []string{
+						"Microsoft.NETCore.Platforms@2.0.0",
+					},
+				},
 			},
 		},
 		{
-			file: "testdata/NoLibraries.deps.json",
-			want: nil,
+			file: "testdata/ExampleApp1.deps.json",
+			wantLibs: []types.Library{
+				{ID: "Newtonsoft.Json@13.0.1", Name: "Newtonsoft.Json", Version: "13.0.1", Locations: []types.Location{{StartLine: 33, EndLine: 39}}},
+				{ID: "ExampleApp1@1.0.0", Name: "ExampleApp1", Version: "1.0.0", Locations: []types.Location{{StartLine: 28, EndLine: 32}}},
+			},
+			wantDeps: []types.Dependency{
+				{
+					ID: "ExampleApp1@1.0.0",
+					DependsOn: []string{
+						"Newtonsoft.Json@13.0.1",
+					},
+				},
+				{
+					ID:        "Newtonsoft.Json@13.0.1",
+					DependsOn: nil,
+				},
+			},
+		},
+		{
+			file:     "testdata/NoLibraries.deps.json",
+			wantLibs: nil,
 		},
 		{
 			file:    "testdata/InvalidJson.deps.json",
@@ -40,30 +272,47 @@ func TestParse(t *testing.T) {
 			f, err := os.Open(tt.file)
 			require.NoError(t, err)
 
-			got, _, err := NewParser().Parse(f)
+			gotLibs, gotDeps, err := NewParser().Parse(f)
 			if tt.wantErr != "" {
 				require.NotNil(t, err)
 				assert.Contains(t, err.Error(), tt.wantErr)
 			} else {
 				require.NoError(t, err)
 
-				sort.Slice(got, func(i, j int) bool {
-					ret := strings.Compare(got[i].Name, got[j].Name)
+				sort.Slice(gotLibs, func(i, j int) bool {
+					ret := strings.Compare(gotLibs[i].Name, gotLibs[j].Name)
 					if ret == 0 {
-						return got[i].Version < got[j].Version
+						return gotLibs[i].Version < gotLibs[j].Version
 					}
 					return ret < 0
 				})
 
-				sort.Slice(tt.want, func(i, j int) bool {
-					ret := strings.Compare(tt.want[i].Name, tt.want[j].Name)
+				sort.Slice(gotDeps, func(i, j int) bool {
+					return gotDeps[i].ID < gotDeps[j].ID
+				})
+
+				for _, dep := range gotDeps {
+					sort.Strings(dep.DependsOn)
+				}
+
+				sort.Slice(tt.wantLibs, func(i, j int) bool {
+					ret := strings.Compare(tt.wantLibs[i].Name, tt.wantLibs[j].Name)
 					if ret == 0 {
-						return tt.want[i].Version < tt.want[j].Version
+						return tt.wantLibs[i].Version < tt.wantLibs[j].Version
 					}
 					return ret < 0
 				})
 
-				assert.Equal(t, tt.want, got)
+				sort.Slice(tt.wantDeps, func(i, j int) bool {
+					return tt.wantDeps[i].ID < tt.wantDeps[j].ID
+				})
+
+				for _, dep := range tt.wantDeps {
+					sort.Strings(dep.DependsOn)
+				}
+
+				assert.Equal(t, tt.wantLibs, gotLibs)
+				assert.Equal(t, tt.wantDeps, gotDeps)
 			}
 		})
 	}
